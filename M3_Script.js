f4101addbf1ab6d36c251788330b82082106646c
John Miller <jamiller40@hotmail.com>
2017-12-10 16:23:08 -0500

    Added stacked bar chart for move specific win/loss/draw percentages

--- M3_Script.js
+++ M3_Script.js
@@ -76,13 +76,156 @@ function loadPieChart(mCountMin, mCountMax){
         .text("Common Opening Moves");
 }
 
+function loadBarGraph(movesShown, data){
+    console.log(data);
+    var svg = d3.select("svg"),
+    margin = {top: 20, right: 20, bottom: 30, left: 80},
+    width = +svg.attr("width") - margin.left - margin.right,
+    height = +svg.attr("height") - margin.top - margin.bottom;
+  
+    var tooltip = d3.select("body").append("div").attr("class", "toolTip");
+  
+    var x = d3.scaleLinear().range([0, width]);
+    var y = d3.scaleBand().range([height, 0]);
+
+    var g = svg.append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        //[move, numMoves, wins, losses, draws]  loss = -1, draw = 0, win = 1
+        var dic = [];
+
+        //win, loss, draw
+        var wins = 0; var losses = 0; var draws = 0;
+
+        //Find every game in which this move was used
+        for(let i = 0; i < data.length; i++){
+            for(let j = 0; j < data[i].moves.length; j++){
+                let n = true;
+
+                let win = false;
+                let loss = false;
+                let draw = false;
+
+                for(let k = 0; k < dic.length; k++){
+                    if(dic[k].move == data[i].moves[j]){
+                        dic[k].count++;
+                        //console.log("Win: " + data[i].Result);
+                        if(data[i].Result === "0-1"){
+                            //console.log(data[i].moves[j] + " 0-1");
+                            win = true;
+                            dic[k].win++;
+                            wins++;
+                        }else if(data[i].Result === "1-0"){
+                            //console.log(data[i].moves[j] + " 1-0");
+                            loss = true;
+                            dic[k].loss++;
+                            losses++;
+                        }else if(data[i].Result === "1/2-1/2"){
+                            //console.log(data[i].moves[j] + " 1/2-1/2");
+                            draw = true;
+                            dic[k].draw++;
+                            draws++;
+                        }
+
+                        //console.log(data[i].moves[j] + " , " + data[i].Result + ", win: " + win);
+                        n = false;
+                        break;
+                    }
+                }
+                if(n){
+                    dic.push({"move": data[i].moves[j], "count" : 1, "win" : 1, "loss" : 1, "draw" : 1});
+                }
+            }
+        }
+
+    console.log("wins: " + wins + ", losses: " + losses + ", draws: " + draws );
+    dic.sort(function(a, b) { return b.count - a.count; });
+    
+    /*for(var i = 0; i < 20; i++){
+        console.log(dic[i]);
+    }*/
+
+    var splicedDic = [];
+    spicedDic = dic.splice(0, 20);
+    console.log(spicedDic.length);
+
+
+    //var svg = d3.select("svg"),
+    margin = {top: 20, right: 20, bottom: 30, left: 40},
+    width = +svg.attr("width") - margin.left - margin.right,
+    height = +svg.attr("height") - margin.top - margin.bottom,
+    g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    var x = d3.scaleBand()
+        .rangeRound([0, width])
+        .paddingInner(0.05)
+        .align(0.1);
+
+    var y = d3.scaleLinear()
+        .rangeRound([height, 0]);
+
+    var z = d3.scaleOrdinal()
+        //Wins, losses, draws
+        .range(["#CCCCCC", "#222222", "#797979"]);
+
+      //for (i = 1, t = 0; i < columns.length; ++i) t += d[columns[i]] = +d[columns[i]];
+      
+     // if (error) throw error;
+
+    var keys = ["win", "loss", "draw"];
+
+    //data.sort(function(a, b) { return b.total - a.total; });
+    x.domain(spicedDic.map(function(d) { return d.move; }));
+    y.domain([0, d3.max(spicedDic, function(d) { return d.count; })]); /*.nice();*/
+    z.domain(keys);
+
+    g.append("g")
+      .selectAll("g")
+      .data(d3.stack().keys(keys)(spicedDic))
+      .enter().append("g")
+        .attr("fill", function(d) { return z(d.key); })
+      .selectAll("rect")
+      .data(function(d) { return d; })
+
+      .enter().append("rect")
+        .attr("x", function(d) { return x(d.data.move); })
+        .attr("y", function(d) { return y(d[1]); })
+        .attr("height", function(d) { console.log(d); /*return y(d.count) - y(d.data.win);*/return y(d[0]) - y(d[1]); })
+        .attr("width", x.bandwidth())
+        
+        .on("mousemove", function(d){
+              tooltip
+                .style("left", d3.event.pageX - 50 + "px")
+                .style("top", d3.event.pageY - 70 + "px")
+                .style("display", "inline-block")
+                .style("position", "absolute")
+                .style("min-width", "80px")
+                .style("height", "auto")
+                .style("background", "none repeat scroll 0 0 #ffffff")
+                .style("padding", "14px")
+                .style("text-align", "center")
+                //Math.round(price / listprice * 100) / 100;
+                .html(d.data.move + ", Wins: " + (Math.round(d.data.win / d.data.count * 100)) + "%, Losses: " + (Math.round(d.data.loss / d.data.count * 100)) + "%, Draws: " + (Math.round(d.data.draw / d.data.count * 100)) + "%");
+                //.html(d.move + ", Wins: " + (d.win) + ", Losses:" + (d.loss) + ", Draws: " + (d.draw));
+          })
+          .on("mouseout", function(d){ tooltip.style("display", "none");})
+
+    g.append("g")
+        .attr("class", "axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
+
+    g.append("g")
+        .attr("class", "axis");
+
+}
 function readData(){
     d3.json("GMallboth(5%).json", function(data){
         theData = data;
-        console.log("HIT")
+        /*console.log("HIT")
         console.log(data);
-        console.log(theData);
-        loadPieChart(0, 8);
+        console.log(theData);*/
+        loadBarGraph(20, theData);
         
         
     });
