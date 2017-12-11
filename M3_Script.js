9732600e2f691ef1e61dacea74ee1b7806fe33fc
Noah Parker <ndp4918@ad.rit.edu>
2017-12-11 14:56:55 -0500

    Heat is visualized
    12/11
    - heat map is visualized
    - scale and location of amount moved is determined
    - board created

--- M3_Script.js
+++ M3_Script.js
@@ -3,25 +3,21 @@ var theData = [];
 var svg;
 var width = 600;
 var height = 600;
-var radius = Math.min(width, height) / 2;
+
 var g;
-
-var pie;
-
-var color = d3.scaleOrdinal(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
-
-var arc;
-
-var path;
-
-var label = d3.arc()
-    .outerRadius(radius - 40)
-    .innerRadius(radius - 40);
 
 
 function loadPieChart(mCountMin, mCountMax){
     
     var dic = [];
+    var radius = Math.min(width, height) / 2;
+    var pie;
+    var color = d3.scaleOrdinal(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+    var arc;
+    var path;
+    var label = d3.arc()
+    .outerRadius(radius - 40)
+    .innerRadius(radius - 40);
     
     for(let i = 0; i < theData.length; i++){
         for(let j = 0; j < theData[i].moves.length && j >= mCountMin && j < mCountMax; j++){
@@ -87,24 +83,23 @@ function loadHeatMap(mCountMin, mCountMax) {
          }
     }
 
-    
     for(let i = 0; i < theData.length; i++){ //use moves
         for(let j = 0; j < theData[i].moves.length && j >= mCountMin && j < mCountMax; j++){
         
             if(theData[i].moves[j] == "O-O" || theData[i].moves[j] == "O-O-O"){ //castling
-                if(theData[i].moves[j] == "O-O" && j%2 == 0){board[0][6]++;} //white castle king side
-                else if(theData[i].moves[j] == "O-O" && j%2 == 1){board[7][6]++;}//black castle king side
-                else if(theData[i].moves[j] == "O-O-O" && j%2 == 0){board[0][2]++;}//white castle queen side
-                else if(theData[i].moves[j] == "O-O-O" && j%2 == 1){board[7][2]++;}//black castle king side
+                if(theData[i].moves[j] == "O-O" && j%2 == 0){board[6][0]++;} //white castle king side
+                else if(theData[i].moves[j] == "O-O" && j%2 == 1){board[6][7]++;}//black castle king side
+                else if(theData[i].moves[j] == "O-O-O" && j%2 == 0){board[2][0]++;}//white castle queen side
+                else if(theData[i].moves[j] == "O-O-O" && j%2 == 1){board[2][7]++;}//black castle king side
             }
             else{ //normal move
                 let xPos = -1;
                 let yPos = -1;
                 for(let k = 1; k < theData[i].moves[j].length; k++){
                     if(!isNaN(parseInt(theData[i].moves[j].charAt(k)))){ //checks to see if it is a number
-                        xPos = theData[i].moves[j].charAt(k-1);
+                        xPos = theData[i].moves[j].charCodeAt(k-1)-97;//ascii for a is 97
                         yPos = parseInt(theData[i].moves[j].charAt(k));
-                        board[yPos][xPos]++;
+                        board[xPos][yPos]++;
                         break;
                     }
                 }
@@ -113,16 +108,52 @@ function loadHeatMap(mCountMin, mCountMax) {
         }
     }
     
+    let maxNum = 0;
+    for (let i = 0; i < 8; i++) {
+         for (let j = 0; j < 8; j++) {
+            if(board[i][j] > maxNum){maxNum = board[i][j]};//determine max, make this better
+
+             let rectangle = g.append("rect")
+                             .attr("width", (width/8))
+                             .attr("height", (height/8))
+                             .attr("x", ((width * i)/8) - (width/2))
+                             .attr("y", ((height * j)/8) - (height/2))
+                             .attr("fill",returnBoardColor(i,j));
+             
+         }
+    }
+    
+    console.log(maxNum);
+    console.log(board);
+    
+    for (let i = 0; i < 8; i++) {//visualize board
+         for (let j = 0; j < 8; j++) {
+             let w = Math.ceil((width/8) * (board[i][j]/maxNum));
+             let h = Math.ceil((height/8) * (board[i][j]/maxNum));
+             let x = Math.floor(((width * i)/8) + (((width/8)-w)/2)) - (width/2);
+             let y = Math.floor(((height * j)/8) + (((height/8)-h)/2)) - (height/2);
+             let rectangle = g.append("rect")
+                             .attr("width", w)
+                             .attr("height", h)
+                             .attr("x", y)
+                             .attr("y", x)
+                             .attr("fill","rgba(255,0,0,.5)");
+         }
+    }
+}
+
+function returnBoardColor(x, y){
+    if((x%2 == 0 && y%2 ==0) || (x%2 == 1 && y%2 == 1)){return "rgba(255,255,255,1)";}//white
+    else {return "rgba(0,0,0,1)";}//black
 }
 
 function readData(){
     d3.json("GMallboth(5%).json", function(data){
         theData = data;
-        console.log("HIT")
-        console.log(data);
+        //console.log(data);
         console.log(theData);
-        loadPieChart(0, 8);
-        
+        //loadPieChart(0, 2);
+        loadHeatMap(0, 6);
         
     });
 }
